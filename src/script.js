f28d4cd0696d7e170e274d03a6d41357e4d2c829
cbrito-l <cbrito-l@student.42.fr>
2022-08-05 11:49:15 +0100

    trying fetch local api

--- src/script.js
+++ src/script.js
@@ -1,13 +1,21 @@
+import fetch from 'node-fetch';
 
  (async function() {
     //const [{ context }] = await( await fetch(`/api/users-get`)).json();
     //
     //document.querySelector('#name').innerHTML = result;
     //
-    let response = await fetch(`/api/users-get`);
+    /* let response = await fetch(`/api/users-get`);
     let data = await response.json();
-    console.log(await data[0].firstName);
-    //document.querySelector('#name').innerHTML = data[0].firstName;
+    console.log(data[0].firstName);
+    document.querySelector('#name').innerHTML = data[0].firstName; */
+    fetch(`/api/users-get`)
+    .then(res => res.json())
+    .then(json => {
+        console.log("First user in the array:");
+        console.log(json[0]);
+        
+})
 }())
 
 
