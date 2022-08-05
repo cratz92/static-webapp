0c88f69036a043da090d202b9599d59dee51c485
cbrito-l <cbrito-l@student.42.fr>
2022-08-05 04:49:00 +0100

    trying some ticks on api

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 
-/* (async function() {
+ (async function() {
     //const [{ context }] = await( await fetch(`/api/users-get`)).json();
     //
     //document.querySelector('#name').innerHTML = result;
@@ -7,8 +7,7 @@
     let response = await fetch(`/api/users-get`);
     let data = await response.json();
     console.log(await data[0].firstName);
-    document.querySelector('#name').innerHTML = data[0].firstName;
+    //document.querySelector('#name').innerHTML = data[0].firstName;
 }())
 
- */
 
